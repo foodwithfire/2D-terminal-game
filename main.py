c9be607cfcd27b325c97dc3693bdaaefcd17746a
firefood <65716578+caca63@users.noreply.github.com>
2023-03-10 17:28:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
+import os, time
 
+
+def print_game():
+    for y in range(w[1]):
+        line = ""
+        if y == 0 or y == w[1] - 1:
+            for x in range(w[0]):
+                line += "*"
+        else:
+            for x in range(w[0]):
+                if x == 0 or x == w[0] - 1:
+                    line += "*"
+                elif x == int(px) and y == int(py):
+                    line += "@"
+                else:
+                    line += " "
+        print(line)
+
+
+px = 17
+py = 1
+g = 0.1
+yvel = 0
+w = (35, 10)
+
+while True:
+    os.system("cls")
+
+    print_game()
+
+    yvel -= g
+    py -= yvel
+
+    if py > w[1] - 2:
+        yvel = 1.2
+
+    time.sleep(0.1)
